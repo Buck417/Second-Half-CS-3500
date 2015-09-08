24756285f64761c71ce73e6535db51243c7db631
Richie Frost <richard.scott.frost@gmail.com>
2015-09-08 10:02:11 -0600

    First commit

---
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    /// <summary>
+    /// This static class evaluates mathematical expressions
+    /// as an 'infix calculator.'
+    /// </summary>
+    public static class Evaluator
+    {
+
+    }
+}
