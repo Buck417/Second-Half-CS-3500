2a156b53e19161cfce5c643b2aa1924008e4f07e
Richie Frost <richard.scott.frost@gmail.com>
2015-09-09 09:42:50 -0600

    Added test class

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FormulaEvaluator
@@ -12,6 +13,20 @@ namespace FormulaEvaluator
     /// </summary>
     public static class Evaluator
     {
+        public delegate int Lookup(string variableName);
 
+        public static int Evaluate<T>(String expression, Lookup variableFinder)
+        {
+            //Make sure there's no whitespace in our expression
+            expression = expression.Replace(" ", string.Empty);
+
+            Stack<T> opStack = new Stack<T>();
+            Stack<T> valStack = new Stack<T>();
+
+            //Tokenize our expression
+            string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            return 0;
+        }
     }
 }
---
+++ PS1/PS1 Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Regular use case
+            string expression = "(2 + 2) * 6";
+
+            //No close parentheses
+            expression = "(2 + 2 * 6";
+        }
+    }
+}
