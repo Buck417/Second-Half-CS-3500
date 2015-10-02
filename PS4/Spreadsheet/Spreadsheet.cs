7a26c0f2a9385d8d38ffbcc92246860654d94d41
Richie Frost <richard.scott.frost@gmail.com>
2015-10-02 01:52:01 -0600

    Cleaned up some of the comments, added others, cleaned up Cell class invariants for Contents and Name

--- PS4/Spreadsheet/Spreadsheet.cs
+++ PS4/Spreadsheet/Spreadsheet.cs
@@ -9,56 +9,16 @@ using SpreadsheetUtilities;
 namespace SS
 {
     /// <summary>
-    /// (Copied from AbstractSpreadsheet for easier development)
-    /// An AbstractSpreadsheet object represents the state of a simple spreadsheet.  A 
-    /// spreadsheet consists of an infinite number of named cells.
-    /// 
-    /// A string is a valid cell name if and only if:
-    ///   (1) its first character is an underscore or a letter
-    ///   (2) its remaining characters (if any) are underscores and/or letters and/or digits
-    /// Note that this is the same as the definition of valid variable from the PS3 Formula class.
-    /// 
-    /// For example, "x", "_", "x2", "y_15", and "___" are all valid cell  names, but
-    /// "25", "2x", and "&" are not.  Cell names are case sensitive, so "x" and "X" are
-    /// different cell names.
-    /// 
-    /// A spreadsheet contains a cell corresponding to every possible cell name.  (This
-    /// means that a spreadsheet contains an infinite number of cells.)  In addition to 
-    /// a name, each cell has a contents and a value.  The distinction is important.
-    /// 
-    /// The contents of a cell can be (1) a string, (2) a double, or (3) a Formula.  If the
-    /// contents is an empty string, we say that the cell is empty.  (By analogy, the contents
-    /// of a cell in Excel is what is displayed on the editing line when the cell is selected.)
-    /// 
-    /// In a new spreadsheet, the contents of every cell is the empty string.
-    ///  
-    /// The value of a cell can be (1) a string, (2) a double, or (3) a FormulaError.  
-    /// (By analogy, the value of an Excel cell is what is displayed in that cell's position
-    /// in the grid.)
-    /// 
-    /// If a cell's contents is a string, its value is that string.
-    /// 
-    /// If a cell's contents is a double, its value is that double.
-    /// 
-    /// If a cell's contents is a Formula, its value is either a double or a FormulaError,
-    /// as reported by the Evaluate method of the Formula class.  The value of a Formula,
-    /// of course, can depend on the values of variables.  The value of a variable is the 
-    /// value of the spreadsheet cell it names (if that cell's value is a double) or 
-    /// is undefined (otherwise).
-    /// 
-    /// Spreadsheets are never allowed to contain a combination of Formulas that establish
-    /// a circular dependency.  A circular dependency exists when a cell depends on itself.
-    /// For example, suppose that A1 contains B1*2, B1 contains C1*2, and C1 contains A1*2.
-    /// A1 depends on B1, which depends on C1, which depends on A1.  That's a circular
-    /// dependency.
+    /// This class encapsulates the behavior and logic of a spreadsheet,
+    /// the Model in our MVC architecture.
     /// </summary>
     public class Spreadsheet : AbstractSpreadsheet
     {
         private DependencyGraph dg;
         private Dictionary<string, Cell> nonEmptyCells;
+
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// Our zero-argument constructor; creates an empty spreadsheet.
+        /// Instantiates a new Spreadsheet, with all empty cells.
         /// </summary>
         public Spreadsheet()
         {
@@ -67,12 +27,10 @@ namespace SS
         }
 
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// If name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, returns the contents (as opposed to the value) of the named cell.  The return
-        /// value should be either a string, a double, or a Formula.
+        /// Gets the contents of a given cell (not the value).
         /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public override object GetCellContents(string name)
         {
             if (!Cell.ValidName(name)) throw new InvalidNameException();
@@ -82,9 +40,10 @@ namespace SS
         }
 
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// Enumerates the names of all the non-empty cells in the spreadsheet.
+        /// Returns an IEnumerable of all of the cells that
+        /// have values in them.
         /// </summary>
+        /// <returns></returns>
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
             foreach(string name in nonEmptyCells.Keys)
@@ -94,21 +53,12 @@ namespace SS
         }
 
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// If the formula parameter is null, throws an ArgumentNullException.
-        /// 
-        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, if changing the contents of the named cell to be the formula would cause a 
-        /// circular dependency, throws a CircularException.  (No change is made to the spreadsheet.)
-        /// 
-        /// Otherwise, the contents of the named cell becomes formula.  The method returns a
-        /// Set consisting of name plus the names of all other cells whose value depends,
-        /// directly or indirectly, on the named cell.
-        /// 
-        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
-        /// set {A1, B1, C1} is returned.
+        /// This sets a cell's contents to have a formula that can be evaluated.
+        /// Dependencies are re-evaluated based on the variables in the formula given.
         /// </summary>
+        /// <param name="name"></param>
+        /// <param name="formula"></param>
+        /// <returns></returns>
         public override ISet<string> SetCellContents(string name, Formula formula)
         {
             if ((object)formula == null) throw new ArgumentNullException();
@@ -136,18 +86,11 @@ namespace SS
         }
 
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// If text is null, throws an ArgumentNullException.
-        /// 
-        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, the contents of the named cell becomes text.  The method returns a
-        /// set consisting of name plus the names of all other cells whose value depends, 
-        /// directly or indirectly, on the named cell.
-        /// 
-        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
-        /// set {A1, B1, C1} is returned.
+        /// This sets a cell's contents to just be a string.
         /// </summary>
+        /// <param name="name"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public override ISet<string> SetCellContents(string name, string text)
         {
             if (text == null) throw new ArgumentNullException();
@@ -165,16 +108,11 @@ namespace SS
         }
 
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// If name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, the contents of the named cell becomes number.  The method returns a
-        /// set consisting of name plus the names of all other cells whose value depends, 
-        /// directly or indirectly, on the named cell.
-        /// 
-        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
-        /// set {A1, B1, C1} is returned.
+        /// This sets a cell's contents to be a static double.
         /// </summary>
+        /// <param name="name"></param>
+        /// <param name="number"></param>
+        /// <returns></returns>
         public override ISet<string> SetCellContents(string name, double number)
         {
             if (!Cell.ValidName(name)) throw new InvalidNameException();
@@ -190,23 +128,11 @@ namespace SS
         }
         
         /// <summary>
-        /// (Copied from AbstractSpreadsheet for easier development)
-        /// If name is null, throws an ArgumentNullException.
-        /// 
-        /// Otherwise, if name isn't a valid cell name, throws an InvalidNameException.
-        /// 
-        /// Otherwise, returns an enumeration, without duplicates, of the names of all cells whose
-        /// values depend directly on the value of the named cell.  In other words, returns
-        /// an enumeration, without duplicates, of the names of all cells that contain
-        /// formulas containing name.
-        /// 
-        /// For example, suppose that
-        /// A1 contains 3
-        /// B1 contains the formula A1 * A1
-        /// C1 contains the formula B1 + A1
-        /// D1 contains the formula B1 - C1
-        /// The direct dependents of A1 are B1 and C1
+        /// Gets the direct dependents of the cell 'name'. I.e., this returns
+        /// all of the cells that depend on 'name'.
         /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
             if (name == null) throw new ArgumentNullException("Cell name cannot be null when searching for direct dependents.");
@@ -218,6 +144,10 @@ namespace SS
         
     }
 
+    /// <summary>
+    /// This class encapsulates some of the exctensions we want to use in
+    /// existing classes.
+    /// </summary>
     public static class Extensions
     {
         /// <summary>
@@ -244,52 +174,44 @@ namespace SS
     /// </summary>
     public class Cell
     {
-        public static Regex rx
-        {
-            //Read-only property
-            get
-            {
-                return new Regex(@"(([a-zA-Z]|[_])+[0-9]*)");
-            }
-        }
+        private static Regex rx = new Regex(@"(([a-zA-Z]|[_])+[0-9]*)");
 
+        /// <summary>
+        /// This helper method just helps us see whether not a given
+        /// name is a valid cell name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public static bool ValidName(string name)
         {
             if (name != null && rx.IsMatch(name) && rx.Match(name).Value.Equals(name)) return true;
             else return false;
         }
-            
+
+        /// <summary>
+        /// The Cell constructor checks that the cell name is valid before
+        /// instantiating a new Cell. If the name isn't valid, throw
+        /// an InvalidNameException.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="contents"></param>
         public Cell(string name, object contents)
         {
+            if (!ValidName(name)) throw new InvalidNameException();
             Name = name;
             Contents = contents;
         }
 
         private string name;
+        /// <summary>
+        /// The name for this cell. Setting the name is private, and
+        /// only done through the constructor.
+        /// </summary>
         public string Name
         {
-            set
+            private set
             {
-                
-                //Make sure it's a valid cell name
-                if (rx.IsMatch(value))
-                {
-                    //If we got a match from the Regex, make sure it's the same as the initial input
-                    if (rx.Match(value).Value.Equals(value))
-                    {
-                        name = value;
-                    }
-                    //If the regexed value isn't the same as the original value, it's not a valid variable
-                    else
-                    {
-                        throw new InvalidNameException();
-                    }
-                }
-                //If there's no regex match at all, it's an invalid variable
-                else
-                {
-                    throw new InvalidNameException();
-                }
+                name = value;
             }
             get
             {
@@ -297,11 +219,14 @@ namespace SS
             }
         }
         private object contents;
+        /// <summary>
+        /// This just returns the cell's contents. The setter is private, and only
+        /// used in the constructor.
+        /// </summary>
         public object Contents
         {
-            set
+            private set
             {
-                //TODO: sanitize the contents setter
                 contents = value;
             }
             get
