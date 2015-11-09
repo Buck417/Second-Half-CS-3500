a9be2cb29167e8012f6443b99c31f77cb690731b
Richie Frost <richard.scott.frost@gmail.com>
2015-11-09 16:30:19 -0700

    Started writing cube and world classes

--- AgCubio/Model/Model.cs
+++ AgCubio/Model/Model.cs
@@ -8,11 +8,43 @@ namespace Model
 {
     public class Cube
     {
-        
+        public int UID, X, Y;
+        public string Color, Name;
+        public bool Food;
+        public double Mass;
+
+        public Cube(int uID, int x, int y, string color, string name, bool food, double mass)
+        {
+            this.UID = uID;
+            this.X = x;
+            this.Y = y;
+            this.Color = color;
+            this.Name = name;
+            this.Food = food;
+            this.Mass = mass;
+        }
     }
 
+    /// <summary>
+    /// This is basically where all the cubes live, as well as the max dimensions for the world.
+    /// </summary>
     public class World
     {
+        int Max_Height, Max_Width;
+        Dictionary<int, Cube> Cubes;
 
+        public World()
+        {
+            Cubes = new Dictionary<int, Cube>();
+        }
+
+        /// <summary>
+        /// Helper method for adding a cube to display in the GUI
+        /// </summary>
+        /// <param name="cube"></param>
+        public void AddCube(Cube cube)
+        {
+            Cubes.Add(cube.UID, cube);
+        }
     }
 }
--- AgCubio/Network_Controller/Network_Controller.cs
+++ AgCubio/Network_Controller/Network_Controller.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Network_Controller
 {
-    public class Network_Controller
+
+    public static class Network_Controller
     {
+
+        /// <summary>
+        /// This guy connects to the server.
+        /// This function should attempt to connect to the server via a provided hostname. It should save the callback function (in a state object) for use when data arrives.
+        ///It will need to open a socket and then use the BeginConnect method. Note this method takes the "state" object and "regurgitates" it back to you when a connection is made, thus allowing "communication" between this function and the Connected_to_Server function.
+        /// </summary>
+        /// <param name="callBack">A function inside the View to be called when a connection is made</param>
+        /// <param name="hostName">The name of the server to connect to</param>
+        /// <returns></returns>
+        public static Socket Connect_To_Server(Delegate callBack, string hostName)
+        {
+            
+            return null;
+        }
+
+        /// <summary>
+        /// This function is referenced by the BeginConnect method above and is "called" by the OS when the socket connects to the server. The "state_in_an_ar_object" object contains a field "AsyncState" which contains the "state" object saved away in the above function.
+        /// Once a connection is established the "saved away" callback function needs to called. Additionally, the network connection should "BeginReceive" expecting more data to arrive(and provide the ReceiveCallback function for this purpose)
+        /// </summary>
+        /// <param name="state_in_an_ar_object"></param>
+        static void Connected_to_Server(IAsyncResult state_in_an_ar_object)
+        {
+
+        }
+
+        /// <summary>
+        /// The ReceiveCallback method is called by the OS when new data arrives. This method should check to see how much data has arrived. If 0, the connection has been closed (presumably by the server). On greater than zero data, this method should call the callback function provided above.
+        /// For our purposes, this function should not request more data.It is up to the code in the callback function above to request more data.
+        /// </summary>
+        /// <param name="state_in_an_ar_object"></param>
+        static void ReceiveCallback(IAsyncResult state_in_an_ar_object)
+        {
+
+        }
+
+        /// <summary>
+        /// This is a small helper function that the client View code will call whenever it wants more data. Note: the client will probably want more data every time it gets data.
+        /// </summary>
+        /// <param name="state"></param>
+        static void i_want_more_data(IAsyncResult state)
+        {
+
+        }
+
+        /// <summary>
+        /// This function (along with it's helper 'SendCallback') will allow a program to send data over a socket. This function needs to convert the data into bytes and then send them using socket.BeginSend.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="data"></param>
+        static void Send(Socket socket, String data)
+        {
+
+        }
+
+        /// <summary>
+        /// This function "assists" the Send function. If all the data has been sent, then life is good and nothing needs to be done (note: you may, when first prototyping your program, put a WriteLine in here to see when data goes out).
+        ///If there is more data to send, the SendCallBack needs to arrange to send this data(see the ChatClient example program).
+        /// </summary>
+        static void SendCallBack()
+        {
+
+        }
     }
 }
