f41232704a6adb4c7473d7661c94fd673dcfda4b
Richie Frost <richard.scott.frost@gmail.com>
2015-11-10 10:31:16 -0700

    Started GUI stuff

--- AgCubio/View/AgCubio_View.Designer.cs
+++ AgCubio/View/AgCubio_View.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AgCubio_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 797);
+            this.Name = "AgCubio_View";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.AgCubioPaint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- AgCubio/View/AgCubio_View.cs
+++ AgCubio/View/AgCubio_View.cs
@@ -12,9 +12,29 @@ namespace View
 {
     public partial class AgCubio_View : Form
     {
+        private System.Drawing.SolidBrush myBrush;
+
         public AgCubio_View()
         {
             InitializeComponent();
+
+            //Use this to prevent screen flickering when redrawing the world
+            DoubleBuffered = true;
+        }
+
+        int count = 0;
+        public void AgCubioPaint(object sender, PaintEventArgs e)
+        {
+            Color color = Color.FromArgb(count, count, count);
+            myBrush = new System.Drawing.SolidBrush(color);
+
+            count++;
+
+            if (count > 255) count = 0;
+
+            e.Graphics.FillRectangle(myBrush, new Rectangle(count, count, 10 + count, 10 + 2 * count));
+            Console.WriteLine("repainting " + count);
+            this.Invalidate();
         }
     }
 }
